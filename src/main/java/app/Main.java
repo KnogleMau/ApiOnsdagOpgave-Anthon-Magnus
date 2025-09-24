5d8a230a2277361addbed18bb43bd1c1969240ec
Istimereal <jjfapir@gmail.com>
2025-09-24 10:33:17 +0200

    poems

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,13 +1,58 @@
 package app;
 
 import app.config.ApplicationConfig;
+import app.dtos.PoemDTO;
+
+import static org.eclipse.jetty.http.HttpURI.build;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Beer Demo");
+        PoemDTO p1 = PoemDTO.builder()
+                .lyric("You and me alone\n" +
+                        "Madness of world locked away\n" +
+                        "Peace and quiet reigns\n")
+                .build();
+
+        PoemDTO p2 = PoemDTO.builder()
+                .lyric("Aching heart and mind\n" +
+                        "Needs touch of a caring soul\n" +
+                        "Yearning company.\n")
+                .build();
+
+        PoemDTO p3 = PoemDTO.builder()
+                .lyric("JANUARY\n" +
+                        "Delightful display\n" +
+                        "Snowdrops bow their pure white heads\n" +
+                        "To the sun's glory.\n")
+                .build();
+
+        PoemDTO p4 = PoemDTO.builder()
+                .lyric("Mellow, mild, May day,\n" +
+                        "calling children out to play.\n" +
+                        "Summer's on her way!\n")
+                .build();
+
+        PoemDTO p5 = PoemDTO.builder()
+                .lyric("Like crunchy cornflakes\n" +
+                        "Gold leaves rustle underfoot\n" +
+                        "Beauty in decay.\n")
+                .build();
+
+        PoemDTO p6 = PoemDTO.builder()
+                .lyric("Strokes of affection\n" +
+                        "Light and tenderly expressed\n" +
+                        "Keep love’s bonds so strong.\n")
+                .build();
+
+        PoemDTO p7 = PoemDTO.builder()
+                .lyric("Yellow flame flickers\n" +
+                        "Shadows dance upon the wall\n" +
+                        "Love grows ever strong.\n")
+                .build();
+
         ApplicationConfig.startServer(7076);
 
     }
