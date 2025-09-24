de8db713be436292250dade484a741b737a94ae8
KnogleMau <mav.solver@hotmail.com>
2025-09-24 09:41:18 +0200

    Bare intial funktioner som kan hjælpe os

--- src/main/java/app/dtos/PoemDTO.java
+++ src/main/java/app/dtos/PoemDTO.java
@@ -1,5 +1,14 @@
 package app.dtos;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+
 public class PoemDTO {
     private int id;
     private String lyric;
--- src/main/java/app/service/DtoToEntity.java
+++ src/main/java/app/service/DtoToEntity.java
@@ -1,5 +1,16 @@
 package app.service;
 
+import app.dtos.PoemDTO;
+import app.entities.Poem;
+
 public class DtoToEntity {
 
+
+    public Poem DtoToEntity(PoemDTO poemDTO){
+
+        return Poem.builder()
+                .id(poemDTO.getId())
+                .lyric(poemDTO.getLyric())
+                .build();
+    }
 }
