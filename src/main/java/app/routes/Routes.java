f31724d25b2776d8d18f46fd749bfc60be0a464e
KnogleMau <mav.solver@hotmail.com>
2025-09-24 10:17:06 +0200

    Lavet en del mere

--- src/main/java/app/routes/Routes.java
+++ src/main/java/app/routes/Routes.java
@@ -3,10 +3,16 @@ package app.routes;
 
 import io.javalin.apibuilder.EndpointGroup;
 
-public class Routes {
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.path;
 
+public class Routes {
+PoemRoutes poemRoutes = new PoemRoutes();
     public EndpointGroup getRoutes(){
 
-        return null;
+        return () -> {
+          get("/", ctx -> ctx.result("Hello World"));
+          path("/poem", poemRoutes.getRoutes());
+        };
     }
 }
